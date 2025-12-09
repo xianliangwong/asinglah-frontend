1b7b4c0d0d2d2daa7776f148213bb03c8ea4f0a9
xianliangwong <xianliangwong0620@gmail.com>
2025-12-10 00:47:28 +0800

    add effects in appconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { loginReducer } from './features/login/login.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { LoginEffects } from './features/login/login.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideStore({
       login: loginReducer,
     }),
+    provideEffects([LoginEffects]),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),

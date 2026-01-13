78e3dd1742630de673bf76dc3ef2942815bdf78f
xianliangwong <xianliangwong0620@gmail.com>
2026-01-13 18:30:22 +0800

    update to expense form component

--- src/app/component/expense-form/expense-form.component.ts
+++ src/app/component/expense-form/expense-form.component.ts
@@ -1,11 +1,50 @@
-import { CommonModule, NgClass } from '@angular/common';
+export interface InitCurrecnyDropDownList {
+  imgString: string;
+  currencyCode: string;
+}
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  ɵInternalFormsSharedModule,
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-expense-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ɵInternalFormsSharedModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './expense-form.component.html',
   styleUrl: './expense-form.component.css',
 })
-export class ExpenseFormComponent {}
+export class ExpenseFormComponent {
+  expenseForm = new FormGroup({ expenseDescription: new FormControl('', Validators.required) });
+
+  currencyDropDown: boolean = false;
+
+  //can change the data source instead of initialize here
+  initCurrencyDropDownList: InitCurrecnyDropDownList[] = [
+    { imgString: 'assets/img/png_jalurgemilang_48px.png', currencyCode: 'MYR' },
+  ];
+
+  placeHolderCurrency!: InitCurrecnyDropDownList;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.placeHolderCurrency = {
+      imgString: 'assets/img/png_jalurgemilang_48px.png',
+      currencyCode: 'MYR',
+    };
+  }
+
+  onSubmit() {}
+
+  toggleDropdown() {
+    this.currencyDropDown = !this.currencyDropDown;
+  }
+}
--- src/app/features/expense-page/expense.state.ts
+++ src/app/features/expense-page/expense.state.ts
@@ -1,7 +1,14 @@
 export interface ExpenseState {
   IsSelectedExpenseForm: boolean;
+  members: ExpenseMembers[] | null;
+}
+
+export interface ExpenseMembers {
+  userId: number;
+  email: string;
 }
 
 export const initState: ExpenseState = {
   IsSelectedExpenseForm: false,
+  members: null,
 };
